8f5ba438448f3346604eca645a4b104d44960146
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2025-07-26 13:29:13 +0300

    Expose companies endpoint

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -8,25 +8,23 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/joshuaalpuerto/go-rest-api/cmd/api/middlewares"
 	"github.com/joshuaalpuerto/go-rest-api/config"
+
+	companycontroller "github.com/joshuaalpuerto/go-rest-api/internals/api/company/controller"
+	companyusecase "github.com/joshuaalpuerto/go-rest-api/internals/api/company/usecase"
 )
 
 // our DI container
 type application struct {
-	conf config.Conf
+	conf         config.Conf
+	repositories struct {
+		companyRepository companyusecase.CompanyRepository
+	}
 	// later we will have storer here and repository
 }
 
-func (app *application) CreateRoutes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
-	return mux
-}
-
-func (app *application) CreateServer(mux http.Handler) error {
+func (app *application) Start(mux http.Handler) error {
 	srv := &http.Server{
 		Addr:    fmt.Sprintf("%s:%s", app.conf.Server.Host, app.conf.Server.Port),
 		Handler: mux,
@@ -54,4 +52,42 @@ func (app *application) CreateServer(mux http.Handler) error {
 	}
 
 	return nil
+}
+func (app *application) Routes() http.Handler {
+	mux := http.NewServeMux()
+	version := app.conf.Version
+
+	companyHandler := companycontroller.NewCompanyHandler(app.repositories.companyRepository)
+
+	mux.HandleFunc(fmt.Sprintf("/%s/companies", version), WrapHandlerWithMiddlewares(companyHandler.GetAllCompanies, middlewares.RequestLogger(), middlewares.CORS()))
+	// mux.HandleFunc(fmt.Sprintf("/%s/companies/{id}", version), companyHandler.GetCompanyByID)
+	// mux.HandleFunc(fmt.Sprintf("/%s/companies", version), companyHandler.CreateCompany)
+	// mux.HandleFunc(fmt.Sprintf("/%s/companies/{id}", version), companyHandler.UpdateCompany)
+	// mux.HandleFunc(fmt.Sprintf("/%s/companies/{id}", version), companyHandler.DeleteCompany)
+
+	// TODO: add here other endpoint
+
+	return mux
+}
+
+// HandlerFunc represents a handler function that returns data and error
+type HandlerFunc func(w http.ResponseWriter, r *http.Request) (any, error)
+
+// WrapHandler wraps a handler function with standard middleware and response handling
+func WrapHandlerWithMiddlewares(handler HandlerFunc, mw ...middlewares.MiddlewareFunc) http.HandlerFunc {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		data, err := handler(w, r)
+		if err != nil {
+			ErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		SuccessResponse(w, data)
+	}
+
+	// check if there are middlewares if yes then chain them, otherwise just call handler
+	if len(mw) > 0 {
+		return middlewares.Chain(h, mw...)
+	}
+
+	return h
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,22 +5,34 @@ import (
 	"os"
 
 	"github.com/joshuaalpuerto/go-rest-api/config"
+	companyusecase "github.com/joshuaalpuerto/go-rest-api/internals/api/company/usecase"
+	"github.com/joshuaalpuerto/go-rest-api/internals/infra/db"
+	infrarepositories "github.com/joshuaalpuerto/go-rest-api/internals/infra/repositories"
 )
 
 // Bootstrap of the application
 func main() {
 	conf := config.New()
+	db, err := db.NewDatabase(conf.DB)
+	if err != nil {
+		fmt.Println("Error creating database:", err)
+		os.Exit(1)
+	}
 
-	// TODO: setup DB connection here.
+	defer db.Close()
+
+	companyRepository := infrarepositories.NewCompanyRepository(db)
 
 	app := &application{
 		conf: conf,
+		repositories: struct {
+			companyRepository companyusecase.CompanyRepository
+		}{
+			companyRepository: companyRepository,
+		},
 	}
 
-	mux := app.CreateRoutes()
-
-	err := app.CreateServer(mux)
-	if err != nil {
+	if err := app.Start(app.Routes()); err != nil {
 		fmt.Println("Error starting server:", err)
 		os.Exit(1)
 	}
